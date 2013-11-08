4dfe76b8f6b2d02b812facad9dcd02331d161e2c
Lee Bock <lbock@esri.com>
2013-11-08 16:42:08 -0500

    regular map (v.s. web map)

--- app/main.js
+++ app/main.js
@@ -9,8 +9,8 @@ dojo.require("esri.map");
 
 var TITLE = "This is the title."
 var BYLINE = "This is the byline"
-var WEBMAP_ID = "3732b8a6d0bc4a09b00247e8daf69af8";
 var GEOMETRY_SERVICE_URL = "http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer";
+var BASEMAP_URL = "http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer";
 
 /******************************************************
 ***************** end config section ******************
@@ -75,29 +75,16 @@ function init() {
 	$("#title").append(TITLE);
 	$("#subtitle").append(BYLINE);	
 
-	var mapDeferred = esri.arcgis.utils.createMap(WEBMAP_ID, "map", {
-		mapOptions: {
-			slider: false,
-			wrapAround180: true,
-			extent:_homeExtent
-		},
-		ignorePopups: false,
-		geometryServiceURL: GEOMETRY_SERVICE_URL
-	});
-	
-	mapDeferred.addCallback(function(response) {	  
+	_map = new esri.Map("map",{slider:false,wrapAround180:false});
+	_map.addLayer(new esri.layers.ArcGISTiledMapServiceLayer(BASEMAP_URL));
 
-		_map = response.map;
-
-		if(_map.loaded){
+	if(_map.loaded){
+		initMap();
+	} else {
+		dojo.connect(_map,"onLoad",function(){
 			initMap();
-		} else {
-			dojo.connect(_map,"onLoad",function(){
-				initMap();
-			});
-		}
-				
-	});
+		});
+	}
 	
 }
 
